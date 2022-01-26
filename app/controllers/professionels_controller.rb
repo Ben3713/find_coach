71d46de7c46c3874e8fa78ad725acedc886a768e
benjamin boganim <benjaminpert@gmail.com>
2022-01-26 13:05:06 +0000

    routecontrollerprofessionel

--- app/controllers/professionels_controller.rb
+++ app/controllers/professionels_controller.rb
@@ -1,22 +1,44 @@
 class ProfessionelsController < ApplicationController
   def index
+    @professionels = Professionel.all
   end
 
   def show
+    @professionel = Professionel.find(params[:id])
   end
 
   def new
+    @professionel = Professionel.new
   end
 
   def create
+    @professionel = Professionel.new(pro_params)
+    if @professionel.save
+      redirect_to professionels_path
+    else
+      render :new
   end
 
   def edit
+     @professionel = Professionel.find(params[:id])
+
+
   end
 
   def update
+     @professionel = Professionel.find(params[:id])
+     @professionel = Professionel.update(pro_params)
+     redirect_to professionel_path(@professionel)
+
   end
 
   def destroy
+    @professionel = Professionel.find(params[:id])
+    @professionel.destroy
+    redirect_to professionels_path
+  end
+  def pro_params
+    params.require(:professionel).permit(:name, :address, :category, :tarif)
+
   end
 end
--- app/models/professionel.rb
+++ app/models/professionel.rb
@@ -1,2 +1,3 @@
 class Professionel < ApplicationRecord
+  has_many :bookings
 end
